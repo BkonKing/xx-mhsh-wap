9b47c9e99ab2e5d1b22ab0ad1d81cb828497053d
BkonKing <33289637+BkonKing@users.noreply.github.com>
2021-06-11 16:54:58 +0800

    fix：任务详情分享

--- src/views/task-detail.vue
+++ src/views/task-detail.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="tf-bg-white tf-body">
-    <van-nav-bar
-      :title="infoData.type_name"
-      :fixed="true"
-      placeholder
-      :border="false"
-    >
+    <van-nav-bar :title="infoData.type_name" :fixed="true" placeholder :border="false">
     </van-nav-bar>
     <div class="tf-body-container">
       <div class="header-block">
-        <div class="task-status bg-FEBF00" :class="{'bg-FE8900': infoData.task_status < 3, 'bg-FF6555' : infoData.task_status == 4, 'bg-ccc' : infoData.task_status > 4}">{{ infoData.task_status_name }}</div>
+        <div
+          class="task-status bg-FEBF00"
+          :class="{
+            'bg-FE8900': infoData.task_status < 3,
+            'bg-FF6555': infoData.task_status == 4,
+            'bg-ccc': infoData.task_status > 4,
+          }"
+        >
+          {{ infoData.task_status_name }}
+        </div>
         <img class="header-bg" src="@/assets/img/task_01.png" />
         <div class="task-tit">{{ infoData.task_title }}</div>
         <div class="task-coin tf-row-vertical-center">
@@ -18,37 +22,21 @@
           <div class="coin-unit">幸福币/人</div>
         </div>
         <div class="task-label tf-row">
-          <div v-for="(item, index) in infoData.task_tag" :key="index" class="lable-item">{{ item }}</div>
+          <div v-for="(item, index) in infoData.task_tag" :key="index" class="lable-item">
+            {{ item }}
+          </div>
         </div>
       </div>
       <div class="body-cont">
         <div class="task-session">
           <div class="session-tit">任务来源</div>
           <div class="release-user tf-row">
-            <img :src="infoData.rwf_avatar" />
+            <img v-if="infoData.rwf_avatar" :src="infoData.rwf_avatar" />
+            <img v-else src="@/assets/imgs/touxiang.png" />
             <div class="tf-row-space-between">
               <div class="release-name van-ellipsis">{{ infoData.rwf_nickname }}</div>
               <div class="release-time">{{ infoData.task_ctime }}</div>
             </div>
-          </div>
-          <div v-if="!isUp" class="complain-tip tf-row-space-between">
-            <div>凡涉及到内容有违法信息传播。收费不合理， 请您警惕并手机关联证据向我们举报。</div>
-            <div @click="complaint">投诉</div>
-          </div>
-        </div>
-        <div v-if="!isUp && receiverInfo && receiverInfo.is_already == 1" class="task-session">
-          <div class="session-tit tf-row-space-between">接单用户<div class="tit-right tf-row-vertical-center" @click="goSchedule">查看<img src="@/assets/img/task_09.png" /></div></div>
-          <div class="release-user tf-row">
-            <img :src="receiverInfo.jdf_avatar" />
-            <div class="tf-row-space-between">
-              <div class="release-name van-ellipsis">{{ receiverInfo.jdf_nickname }}</div>
-              <div class="release-time" :class="{'color-FF5240' : receiverInfo.progress_status != 2}">{{ receiverInfo.progress_status_name }}</div>
-            </div>
-            <div @click="deliverTask" v-if="receiverInfo.is_deliver == 1" class="finish-btn">交付任务</div>
-          </div>
-          <div v-if="receiverInfo.taskflow_list.length" class="progress-step">
-            <div v-for="(item, index) in receiverInfo.taskflow_list" :key="index">{{ item.ctime + ' ' + item.progress_title }}</div>
-            <!-- <div>03-21 12:00 接单成功，任务进行中</div> -->
           </div>
         </div>
         <div class="task-session">
@@ -56,24 +44,65 @@
           <div class="ask-list">
             <div class="ask-item">需要人数：{{ infoData.need_people_text }}</div>
             <div class="ask-item">完成时间：{{ infoData.task_time }}</div>
-            <div v-if="isUp" class="ask-item">可见范围：{{ infoData.range_type_name }}</div>
+            <div class="ask-item">可见范围：{{ infoData.range_type_name }}</div>
           </div>
         </div>
         <div v-if="infoData.address" class="task-session">
           <div class="session-tit">完成地点</div>
-          <div class="task-address tf-vertical-center">
-            <div class="van-ellipsis">{{ infoData.address }}</div>
-            <div class="van-ellipsis">{{ infoData.address_province + ' ' + infoData.address_city + ' ' + infoData.address_area }}</div>
-            <img class="address-arrow" src="@/assets/img/task_04.png" />
+          <div class="task-address">
+            <div class="task-address-left">
+              <div>
+                {{ infoData.udpate_address || infoData.address }}
+              </div>
+              <div class="van-ellipsis">
+                {{
+                  infoData.address_province +
+                    ' ' +
+                    infoData.address_city +
+                    ' ' +
+                    infoData.address_area
+                }}
+              </div>
+            </div>
+            <div class="task-address-right">
+              <img class="address-arrow" src="@/assets/img/task_04.png" />
+            </div>
             <img class="address-bg" src="@/assets/img/task_03.png" />
           </div>
         </div>
         <div class="task-session">
-          <div class="session-tit tf-row-space-between">任务说明<span class="tit-right" v-if="infoData.renew_day > 0">更新：{{ infoData.renew_day }}天前</span></div>
+          <div class="session-tit tf-row-space-between">
+            任务说明<span class="tit-right" v-if="infoData.renew_day"
+              >更新：{{ infoData.renew_day }}</span
+            >
+          </div>
           <div class="detai-cont">
-            <div class="cont-text" :class="{'text-hidden': isOver&&!isDown}" ref="textCont">{{ infoData.task_desc }}<div @click="showToggle" v-show="isOver" class="more-down" :class="{'down-up' : isDown}">{{ isDown ? '收起' : '展开' }}</div></div>
-            <div v-if="infoData.task_image && infoData.task_image.length" class="cont-pic tf-row-wrap">
-              <img @click="previewPic(index)" v-for="(item, index) in infoData.task_image" :key="index" :src="item" />
+            <div class="text-block">
+              <div class="cont-text" :class="{ 'text-hidden': isOver && !isDown }" ref="textCont">
+                <div
+                  @click="showToggle"
+                  v-show="isOver"
+                  class="more-down"
+                  :class="{ 'down-up': isDown }"
+                >
+                  {{ isDown ? '收起' : '展开' }}
+                </div>
+                <div
+                  class="task-desc-box"
+                  v-html="infoData.task_desc.replace(/\r\n|\n/g, '<br/>')"
+                ></div>
+              </div>
+            </div>
+            <div
+              v-if="infoData.task_image && infoData.task_image.length"
+              class="cont-pic tf-row-wrap"
+            >
+              <img
+                @click="previewPic(index)"
+                v-for="(item, index) in infoData.task_image"
+                :key="index"
+                :src="item"
+              />
             </div>
           </div>
         </div>
@@ -86,7 +115,7 @@
           :extinfo="extinfo"
           @error="handleErrorFn"
           @launch="handleLaunchFn"
-          >
+        >
           <script type="text/wxtag-template">
             <div class="btn" style="color: #fff;
             font-size: 16px">在APP内打开</div>
@@ -94,16 +123,20 @@
         </wx-open-launch-app>
       </button>
       <button v-else class="app-btn" @click="goApp">在APP内打开</button>
-      <confirm-model v-model="confirmShow" :modelTit="confirm.modelTit" :modelSubTit="confirm.modelSubTit" :cancelTxt="confirm.cancelTxt" :yesTxt="confirm.yesTxt" :cancel="confirm.cancel"></confirm-model>
+      <confirm-model
+        v-model="confirmShow"
+        :modelTit="confirm.modelTit"
+        :modelSubTit="confirm.modelSubTit"
+        :cancelTxt="confirm.cancelTxt"
+        :yesTxt="confirm.yesTxt"
+        :cancel="confirm.cancel"
+      ></confirm-model>
     </div>
   </div>
 </template>
 
 <script>
-import {
-  NavBar,
-  ImagePreview
-} from 'vant'
+import { NavBar, ImagePreview } from 'vant'
 import { getTaskInfo } from '@/api/share.js'
 import confirmModel from '../components/confirm-model'
 import { openApp, isWx, txJssdk } from '@/utils/util.js'
@@ -118,9 +151,9 @@ export default {
       taskId: '',
       isOver: false, // 内容是否超出行数
       isDown: false,
-      infoData: '',
-      receiverInfo: '',
-      isUp: true, // 是否是发布者
+      infoData: {
+        task_desc: ''
+      },
       confirmShow: false,
       confirm: {
         modelTit: '是否确定接单？',
@@ -129,7 +162,6 @@ export default {
         yesTxt: '接单',
         cancel: true
       },
-      isBack: false,
       shieldInfo: {}, // 屏蔽信息
       extinfo: '',
       isWx: false
@@ -142,12 +174,10 @@ export default {
     this.isWx = isWx()
     txJssdk()
   },
-  mounted () {
-
-  },
+  mounted () {},
   methods: {
     getData () {
-      getTaskInfo({ linli_task_id: this.taskId }).then(res => {
+      getTaskInfo({ linli_task_id: this.taskId }).then((res) => {
         this.shieldInfo = {
           uid: res.data.uid,
           nickname: res.data.rwf_nickname,
@@ -155,11 +185,7 @@ export default {
           id: res.data.task_id
         }
         this.infoData = res.data
-        if (!this.isUp) {
-          this.receiverInfo = res.data.user_task_data
-        }
-        if (!this.isUp && res.popup_data.is_popup == 1) {
-          this.isBack = true
+        if (res.popup_data.is_popup == 1) {
           this.confirmShow = true
           this.confirm.modelTit = res.popup_data.popup_text
           this.confirm.modelSubTit = res.popup_data.popup_text2
@@ -173,8 +199,7 @@ export default {
     },
     getTextOver () {
       const textCont = this.$refs.textCont
-      const textHeight = textCont.clientHeight * 750 / document.documentElement.clientWidth
-      console.log(textHeight)
+      const textHeight = (textCont.clientHeight * 750) / document.documentElement.clientWidth
       if (textHeight > 48 * 6) {
         this.isOver = true
       }
@@ -194,51 +219,37 @@ export default {
         }
       })
     },
-    // 分享
-    share () {
-      this.$refs.taskop.share()
-    },
     // 跳转到app
     goApp () {
       const params = 'page_type=5&taskId=' + this.taskId
       openApp(params)
-    }
-  },
-  watch: {
-    confirmShow (val) {
-      console.log(111, val, this.isBack)
-      if (!val) {
-        if (this.isBack) {
-          this.$router.go(-1)
-        }
-      }
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
-.tf-body-container{
+.tf-body-container {
   font-size: 28px;
   color: #333;
-  background-color: #F7F7F7;
+  background-color: #f7f7f7;
 }
 /deep/.van-nav-bar {
-  background-color: #FFD34D;
+  background-color: #ffd34d;
   .van-nav-bar__right .van-icon {
     font-size: 44px;
   }
 }
 .header-block {
-  background-color: #F7F7F7;
+  background-color: #f7f7f7;
   padding: 38px 0 20px 50px;
-  background: linear-gradient(0deg, #FEBF00 0%, #FFD34D 100%);
+  background: linear-gradient(0deg, #febf00 0%, #ffd34d 100%);
   position: relative;
   margin-bottom: 30px;
   .task-tit {
     font-size: 44px;
-    font-weight: 500;
-    color: #2A334A;
+    font-weight: bold;
+    color: #2a334a;
     line-height: 60px;
     width: 486px;
     margin-bottom: 10px;
@@ -246,7 +257,7 @@ export default {
   .task-coin {
     height: 56px;
     line-height: 56px;
-    color: #2A334A;
+    color: #2a334a;
     margin-bottom: 30px;
     img {
       width: 36px;
@@ -255,7 +266,7 @@ export default {
     }
     .coin-num {
       font-size: 40px;
-      font-weight: 500;
+      font-weight: bold;
       margin-right: 10px;
     }
     .coin-unit {
@@ -265,14 +276,15 @@ export default {
   .task-label {
     position: relative;
     z-index: 5;
+    flex-wrap: wrap;
     .lable-item {
-      background-color: #FCD86E;
+      background-color: #fcd86e;
       height: 48px;
       line-height: 48px;
       border-radius: 10px;
       padding: 0 18px;
       font-size: 24px;
-      color: #A46400;
+      color: #a46400;
       margin: 0 10px 20px 0;
     }
   }
@@ -284,29 +296,30 @@ export default {
     height: 241px;
   }
   .task-status {
+    display: flex;
+    justify-content: center;
+    align-items: center;
     position: absolute;
     right: 0;
     top: 38px;
     width: 140px;
     height: 64px;
-    line-height: 64px;
-    text-align: center;
     border-radius: 32px 0px 0px 32px;
     font-size: 26px;
     font-weight: 500;
-    color: #FFFFFF;
+    color: #ffffff;
     z-index: 3;
     &.bg-ccc {
-      background: #CCCCCC;
+      background: #cccccc;
     }
     &.bg-FF6555 {
-      background: #FF6555;
+      background: #ff6555;
     }
     &.bg-FEBF00 {
-      background: #FEBF00;
+      background: #febf00;
     }
     &.bg-FE8900 {
-      background: #FE8900;
+      background: #fe8900;
     }
   }
 }
@@ -319,14 +332,14 @@ export default {
   border-radius: 10px;
   .session-tit {
     font-size: 32px;
-    font-weight: 500;
+    font-weight: bold;
     color: #000000;
     height: 100px;
     padding-top: 10px;
     line-height: 90px;
     .tit-right {
       font-size: 24px;
-      color: #8F8F94;
+      color: #8f8f94;
       line-height: 90px;
       img {
         width: 36px;
@@ -352,16 +365,16 @@ export default {
     flex-direction: column;
   }
   .release-name {
-    font-weight: 500;
+    font-weight: bold;
     color: #000000;
     line-height: 36px;
   }
   .release-time {
     font-size: 24px;
-    color: #8F8F94;
+    color: #8f8f94;
     line-height: 30px;
     &.color-FF5240 {
-      color: #FF5240;
+      color: #ff5240;
     }
   }
   .finish-btn {
@@ -369,10 +382,10 @@ export default {
     text-align: center;
     height: 64px;
     line-height: 64px;
-    background: #FF6555;
+    background: #ff6555;
     border-radius: 10px;
     font-size: 24px;
-    color: #FFFFFF;
+    color: #ffffff;
     justify-content: flex-start;
     flex-shrink: 0;
   }
@@ -386,37 +399,42 @@ export default {
   }
 }
 .task-address {
-  height: 128px;
-  background: linear-gradient(90deg, #F7F7F7 0%, #FFFFFF 100%);
+  display: flex;
+  justify-content: space-between;
+  min-height: 128px;
+  background: linear-gradient(90deg, #f7f7f7 0%, #ffffff 100%);
   border-radius: 10px;
   margin-bottom: 40px;
   position: relative;
-  padding-left: 30px;
-  div {
+  padding: 20px 30px;
+  .task-address-left {
+    div {
+      position: relative;
+      z-index: 5;
+      line-height: 44px;
+      max-width: 530px;
+    }
+    div:nth-child(1) {
+      font-size: 26px;
+      color: #000000;
+    }
+    div:nth-child(2) {
+      font-size: 24px;
+      color: #8f8f94;
+    }
+  }
+  .task-address-right {
+    display: flex;
+    align-items: center;
     position: relative;
-    z-index: 5;
-    line-height: 44px;
-    max-width: 530px;
-  }
-  div:nth-child(1) {
-    font-size: 26px;
-    color: #000000;
-  }
-  div:nth-child(2) {
-    font-size: 24px;
-    color: #8F8F94;
-  }
-  img {
-    position: absolute;
-  }
-  .address-arrow {
-    right: 30px;
-    top: 40px;
-    width: 48px;
-    height: 48px;
-    z-index: 3;
+    .address-arrow {
+      width: 48px;
+      height: 48px;
+      z-index: 1;
+    }
   }
   .address-bg {
+    position: absolute;
     right: 0;
     top: 0;
     width: 314px;
@@ -424,8 +442,11 @@ export default {
   }
 }
 .detai-cont {
+  .text-block {
+    display: flex;
+  }
   .cont-text {
-    color: #8F8F94;
+    color: #8f8f94;
     line-height: 48px;
     font-size: 30px;
     position: relative;
@@ -433,6 +454,11 @@ export default {
     &.text-hidden {
       height: 288px;
       overflow: hidden;
+    }
+    &::before {
+      content: '';
+      float: right;
+      height: calc(100% - 48px); /*先随便设置一个高度*/
     }
   }
   .cont-pic {
@@ -451,35 +477,36 @@ export default {
 .complain-tip {
   height: 112px;
   align-items: center;
-  background: #F7F7F7;
+  background: #f7f7f7;
   margin: -10px 0 40px;
   padding: 0 16px 0 28px;
   font-size: 24px;
   div:nth-child(1) {
     width: 480px;
-    color: #8F8F94;
+    color: #8f8f94;
   }
   div:nth-child(2) {
     width: 100px;
     height: 48px;
     text-align: center;
     line-height: 44px;
-    border: 1PX solid #FF6555;
+    border: 1px solid #ff6555;
     border-radius: 10px;
-    color: #FF6555;
+    color: #ff6555;
   }
 }
 .progress-step {
-  background: #FFF6F5;
+  background: #fff6f5;
   border-radius: 10px;
   padding: 28px 20px 28px 0;
   font-size: 24px;
-  color: #8F8F94;
+  color: #8f8f94;
   margin-bottom: 40px;
   div {
     line-height: 48px;
     position: relative;
     padding-left: 60px;
+    @text-ellipsis();
     &::before {
       content: '';
       position: absolute;
@@ -487,7 +514,7 @@ export default {
       top: 19px;
       width: 10px;
       height: 10px;
-      background: #8F8F94;
+      background: #8f8f94;
       border-radius: 50%;
     }
     &:first-child {
@@ -513,5 +540,49 @@ export default {
   text-align: center;
   z-index: 2;
   box-shadow: 0.15rem 0.15rem 0.3rem #999;
+}
+.text-hidden {
+  .more-down {
+    margin-left: 20px;
+  }
+  .more-down::before {
+    display: block;
+  }
+}
+.more-down {
+  position: relative;
+  font-size: 24px;
+  height: 48px;
+  line-height: 48px;
+  color: #0089c9;
+  // background-color: #fff;
+  padding-left: 40px;
+  float: right;
+  clear: both;
+  margin-right: 20px;
+  &.down-up::after {
+    border-width: 0 7px 12px 7px;
+    border-color: transparent transparent #0089c9 transparent;
+  }
+  &::after {
+    content: '';
+    width: 0;
+    height: 0;
+    position: absolute;
+    left: 14px;
+    top: 50%;
+    transform: translate(0, -50%);
+    border-style: solid;
+    border-width: 12px 7px 0 7px;
+    border-color: #0089c9 transparent transparent transparent;
+  }
+  &::before {
+    display: none;
+    content: '...';
+    position: absolute;
+    left: -5px;
+    color: #8f8f94;
+    transform: translateX(-100%);
+  }
 }
 </style>
